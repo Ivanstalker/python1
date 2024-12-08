e7fe2002bea19c0ac248d15fb1a178ea79c0ecf7
ivan stalker <kiv_krs@mail.ru>
2024-12-08 19:48:18 +0200

    init

---
+++ 1.py
@@ -0,0 +1,45 @@
+import math
+# # a = int(input('enter the first number: '))
+# # b = int(input('enter the second number: '))
+# #
+# # print(a - b)
+# # print(a + b)
+# # print(a * b)
+# # print(a / b)
+# #
+# # r =  float(input('enter the radius of your circle: '))
+# # p = 3.14
+# # r2 = r * r
+# # print(p * r2)
+#
+# # name = input('enter your name: ')
+# # hello = 'Hello'
+# # print(name.upper() + " , " + hello)
+#
+# a = float(input('enter the number: '))
+# if a < 0 :
+# 	print(f'the number {a} is false')
+# elif a >= 0 :
+# 	print(f'the number {a} is true')
+#
+# print('enter the 5 numbers: ')
+# nums = list()
+# for i in range(5):
+# 	nums.append(float(input()))
+# print(nums[:: -1])
+#
+# import math
+# r =  float(input('enter the radius of your circle: '))
+# p = math.pi
+# print(f'the ploshad and leuight of circle is :{p * r ** 2}, {2 * p * r}')
+
+a = float(input('enter the first number: '))
+b = float(input('enter the second number: '))
+
+if a < b :
+	print(f'number {b} > number {a}')
+elif a > b:
+	print(f'number {a} > number {b}')
+elif a == b :
+	print(f'numbers are =')
+	
